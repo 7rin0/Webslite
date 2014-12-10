c2eb018120bc6cbaa4ce57fcec6d5f8cfc07e3f3
Luis SEVERINO <l.severino@c2is.fr>
2014-12-10 08:52:52 +0100

    Day 3

--- webslite.js
+++ webslite.js
@@ -10,30 +10,11 @@ function getImage_facebook () {
 
     var imgs = $('img'),
         body = $( 'body'),
-        outImagem = '',
-        img_src = '',
-        img_width = '',
-        hasSlide = body.hasClass( 'slide'),
-        increment = 0;
+        hasSlide = body.hasClass( 'slide');
 
     imgs.each( function ( index ) {
-        img_src = $( this ).attr('src');
-        img_width = $( this ).width();
-        outImagem = '<img src="'+ img_src +'" />';
-
-        // Validate Images
-        if ( img_width > 200 ) {
-            increment++;
-            $('.fotorama').append(outImagem);
-
-            console.log(img_src);
-            //console.log( $.get( img_src ) );
-        }
-
-        // Limit Images
-        if (increment > 20) {
-            return false;
-        }
+        var image = $(this);
+        generateSlide(image);
     });
 
     // Show Slider When All Content Loaded
@@ -41,8 +22,92 @@ function getImage_facebook () {
         body.addClass('slide');
         $('#webslider').delay(5000).fadeIn(1000);
     }
+
+    // Rise up
+    $('.fotorama').fotorama();
+    $('html, body').animate({scrollTop: 0}, 3000);
 }
 
-// Load Extension After Other Scripts
-$( 'body' ).prepend( "<div id='webslider' style='display: none; margin:5% auto; width: 400px'><div class='fotorama' data-width='100%' data-ratio='800/600'></div></div>" );
-getImage_facebook();
+/** startWebslite(); **/
+
+/***********************************************************************************************************/
+
+// Images from noscript
+function noscripts () {
+
+    var objectos = $('noscript');
+
+    objectos.each( function ( index ) {
+        var image = $(this);
+
+        generateSlide ( image );
+        //generateLog ( image );
+
+        if ( index > 100 ) {
+            return false;
+        }
+    });
+
+}
+
+// Generate and Appends to Slide
+function generateSlide ( image ) {
+    // Declare Vars
+    var outImagem ,
+        img_src ,
+        img_alt ,
+        img_width ,
+        img_height ;
+
+    /**if ( image != '' && $.type( image ) === 'object' ) {
+        if (image.attr('src') != 'undefined') {*/
+            // Setters
+            img_src = image.attr('src');
+            img_alt = image.attr('alt');
+            img_width = image.width();
+            img_height = image.height();/**
+        }
+
+        if (image.attr('data-src') != 'undefined') {
+            img_src = image.attr('data-src');
+            img_alt = image.attr('data-alt');
+            img_width = 200;
+            img_height = 200;
+        }
+    }*/
+
+    // Getter
+    outImagem = '<a href="'+ img_src +'" data-caption="'+ img_alt +'" ></a>';
+
+    // Validate Images and Append
+    if ( img_width > 150 && img_height > 150 ) {
+        $('.fotorama').append(outImagem);
+    }
+}
+
+// Generate Log For Each
+function generateLog ( objecto ) {
+    console.log( objecto );
+}
+
+// Start Webslite
+function startWebslite () {
+    // Append Slide
+    $( 'body' ).prepend( "<div id='webslider' style='display: none; margin:5% auto; max-width: 600px'><div class='fotorama' data-width='100%' data-auto='false' data-arrows='true' data-keyboard='true' data-arrows='always' data-ratio='16/9' data-allowfullscreen='native' data-nav='thumbs'></div></div>" );
+    // Lazy-Load-Killer
+    $('html, body').animate({scrollTop: 100000}, 20000);
+    setTimeout( function () {
+        getImage_facebook();
+    }, 20000);
+}
+
+// Starters By DOcument Ready
+$( document).ready( function () {
+    /**
+    $( 'body' ).prepend( "<div id='webslider' style='display: none; margin:5% auto; max-width: 600px'><div class='fotorama' data-width='100%' data-auto='false' data-arrows='true' data-keyboard='true' data-arrows='always' data-ratio='16/9' data-allowfullscreen='native' data-nav='thumbs'></div></div>" );
+    $('.fotorama').fotorama();
+    noscripts();
+     */
+
+});
+startWebslite();

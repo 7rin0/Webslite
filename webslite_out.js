d9dddddef8186963b40e34a7c95e6808d283ffd2
Luis SEVERINO <lfs.severino@gmail.com>
2016-03-06 14:24:28 +0100

    restructure webslite extension

--- webslite_out.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by lseverino on 10/12/14.
- */
-
-//****************** startWebslite ******************//
-var scrollDetector = $(document).height() * 15;
-
-getWebslite( scrollDetector );
